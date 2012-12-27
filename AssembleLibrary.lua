4da77f2b052e7d50fa66b3f572f9e65b247efa56
Anaminus <anaminus@no-email>
2012-12-27 00:00:52 -0600

    file name is now based on root library name, if unspecified; switched arguments of assembler function; now only describes usage, see README for full documentation

--- AssembleLibrary.lua
+++ AssembleLibrary.lua
@@ -3,34 +3,27 @@ Lua Library Assembler
 
 Attempts to consolidate the many files of a Lua library into a single Lua file.
 
-This works by replacing the Lua library searcher function in package.loaders
-with a proxy function. This pretty much does the same thing as the former,
-except it also reads the contents of a found file, which gets added to the
-assembled file.
-
-This module sets up the searcher function, and returns a function that, when
-called with a file name as an argument, allows the assembled content to be
-written to the given file. So, the general prodecure for assembling a library
-is to require the assembler, require the library you wish to assemble, then
-call the assembler with the file to output to. Example:
-
+Usage:
     require 'AssembleLibrary'
 
     -- do stuff normally
     require 'library'
     -- etc
 
-    require 'AssembleLibrary' ("library-assembled.lua")
+    require 'AssembleLibrary' ()
+    -- outputs to 'library.lua' with 'library' as root
 
-A second argument to the assembler function specifies the root library, which
-allows the assembled file to be used as a library itself. If no second
-argument is given, the name of the first library that was required will be
-used.
+Specify root library:
 
-If a library happens to load dependencies only when needed, then you will have
-to use it in whatever way necessary to make sure they are loaded. If the
-library comes with a unit test, that should do the job.
+    require 'AssembleLibrary' ('library.sublib.subfunc')
 
+Specify exact name of output file:
+
+    require 'AssembleLibrary' (nil, 'library.lua')
+
+Specify both:
+
+    require 'AssembleLibrary' ('library.sublib.subfunc', 'library.lua')
 
 ]]
 
@@ -109,9 +102,10 @@ do	-- replace lua library searcher
 	end
 end
 
-return function(filename,rootLib)
+return function(rootLib,fileName)
 	rootLib = rootLib or firstLib
-	local file = io.open(filename,'w')
+	fileName = fileName or (rootLib .. '.lua')
+	local file = io.open(fileName,'w')
 	if file then
 		file:write(assembledFile)
 		file:write(string.format("return Libraries[%q](%q)\n",rootLib,rootLib))
