cd24a983c40160b699f2751af613d7d2c03b89a9
Anaminus <anaminus@no-email>
2012-12-26 23:20:02 -0600

    output file can now be used as a library

--- AssembleLibrary.lua
+++ AssembleLibrary.lua
@@ -1,38 +1,48 @@
 --[[
 Lua Library Assembler
 
-Attempts to gather dependent Lua libraries into a single Lua file.
+Attempts to consolidate the many files of a Lua library into a single Lua file.
 
 This works by replacing the Lua library searcher function in package.loaders
 with a proxy function. This pretty much does the same thing as the former,
 except it also reads the contents of a found file, which gets added to the
 assembled file.
 
-This returns a function that, when called with a file name as an argument,
-allows the assembled content to be written to the given file. So, the general
-prodecure for assembling a library is to require the assembler, with an output
-filename, then require the library you wish to assemble. Example:
+This module sets up the searcher function, and returns a function that, when
+called with a file name as an argument, allows the assembled content to be
+written to the given file. So, the general prodecure for assembling a library
+is to require the assembler, require the library you wish to assemble, then
+call the assembler with the file to output to. Example:
 
-    require 'AssembleLibrary' "library-assembled.lua"
+    require 'AssembleLibrary'
+
     -- do stuff normally
     require 'library'
     -- etc
+
+    require 'AssembleLibrary' ("library-assembled.lua")
+
+A second argument to the assembler function specifies the root library, which
+allows the assembled file to be used as a library itself. If no second
+argument is given, the name of the first library that was required will be
+used.
 
 If a library happens to load dependencies only when needed, then you will have
 to use it in whatever way necessary to make sure they are loaded. If the
 library comes with a unit test, that should do the job.
 
+
 ]]
 
-local outputFile = nil
+local firstLib
 local assembledFile = [==[
 local loaderEnv = {}
 for k,v in pairs(_G) do loaderEnv[k] = v end
 
-local LuaLibraries = {}
+local Libraries = {}
 table.insert(loaderEnv.package.loaders,1,function(name)
-	if LuaLibraries[name] then
-		return LuaLibraries[name]
+	if Libraries[name] then
+		return Libraries[name]
 	else
 		return "\n\tno module '"..name.."'"
 	end
@@ -40,7 +50,7 @@ end)
 
 function loader(name,func)
 	setfenv(func,loaderEnv)
-	LuaLibraries[name] = func
+	Libraries[name] = func
 end
 
 ----------------------------------------------------------------
@@ -59,15 +69,11 @@ end)
 
 local loadedModules = {}
 local function appendModuleContent(name,content)
+	if not firstLib then
+		firstLib = name
+	end
 	if not loadedModules[name] then
-		local wrapper = string.format(moduleWrapper,name,content)
-		if outputFile then
-			outputFile:write(wrapper)
-			outputFile:flush()
-		else
-			-- do this in case the user chooses not to call the assemble function immediately
-			assembledFile = assembledFile .. wrapper
-		end
+		assembledFile = assembledFile .. string.format(moduleWrapper,name,content)
 		loadedModules[name] = true
 	end
 end
@@ -103,12 +109,13 @@ do	-- replace lua library searcher
 	end
 end
 
-return function(filename)
-	if not outputFile then
-		outputFile = io.open(filename,'w')
-		if outputFile then
-			outputFile:write(assembledFile)
-			outputFile:flush()
-		end
+return function(filename,rootLib)
+	rootLib = rootLib or firstLib
+	local file = io.open(filename,'w')
+	if file then
+		file:write(assembledFile)
+		file:write(string.format("return Libraries[%q](%q)\n",rootLib,rootLib))
+		file:flush()
+		file:close()
 	end
 end
